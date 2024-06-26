6dfaddcc0a42c91a96beb1c11e13cd733b36c0c9
yuyeon <yuyeon48@naver.com>
2024-06-26 22:59:27 +0900

    first commit

---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User 
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+class UserSerializer(ModelSerializer):
+    email = serializers.EmailField(required=True)
+    name = serializers.CharField(write_only=True)
+
+    def create(self, validated_data): 
+        name = validated_data.pop('name')
+        user = User.objects.create_user(     
+            email=validated_data['email'],         
+            username = validated_data['username'], 
+            password = validated_data['password']   
+        )
+        user.first_name = name
+        user.save()
+        return user  
+    class Meta:
+        model = User
+        fields = [ 'username',  'email','password' ,'name'] 
