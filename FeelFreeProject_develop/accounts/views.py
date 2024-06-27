54c9067f4ace85f44b4d74e48d78bcfa0800fc57
likelionTeamOne <likelionteamone@gmail.com>
2024-06-27 15:55:26 +0900

    accounts 회원정보 권한 관련 문제 해결 (삭제버튼 이슈)(
    예를들어, user1은 user1의 회원정보를 읽을 수 있고, 수정할 수 있다.(수정 시, current_password를 통해 한번 더 본인인증 함)
    user2는 user1의 회원정보를 읽을수도 없고, 수정할 수도 없다. 
    user1, user2 모두 자신의 회원정보를 삭제할 수 없다.

--- FeelFreeProject_develop/accounts/views.py
+++ FeelFreeProject_develop/accounts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, UserUpdateSerializer
 from rest_framework import viewsets
+from .permissions import IsOwner
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
+    permission_classes = [IsOwner]
 
     def get_serializer_class(self):
         if self.action == 'create':
